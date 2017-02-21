ddcea6438926f00f3d2443a85c8ff0e058bfe8ff
Icmus23 <i.cmus@mail.ru>
2017-02-21 17:09:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+phone_checker
+=============
+
+A Symfony project created on February 21, 2017, 4:38 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $host = 'https://mi.ua';
+        $uri = '/mi-phones/smartfon-xiaomi-redmi-4-dark-gray-332-gb-ukrainska-versiya/';
+
+        $client = new Client([
+            'base_uri' => $host,
+            'timeout'  => 2.0,
+        ]);
+
+        $response = $client->request('GET', $uri);
+
+        $pageContent = (string) $response->getBody();
+
+        $pattern = "/<div class=\"product-stock\"><i .*><\/i>(.*)<\/div>/";
+        preg_match($pattern, $pageContent, $matches);
+
+        $result = isset($matches[1]) ? $matches[1] : '';
+
+        return new Response($result.' <a href="'.$host.$uri.'">В магазин</a>');
+    }
+}
