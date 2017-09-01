b9b1095e759f7885b4edf366d2fefdae79f41e07
Nikolay Ustimenko <icmus.mail@gmail.com>
2017-09-01 12:43:05 +0300

    comments3

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,7 +27,7 @@ class DefaultController extends Controller
         $response = $client->request('GET', $uri);
         // page content
         $pageContent = (string) $response->getBody();
-
+        // pattern
         $pattern = "/<div class=\"product-stock\"><i .*><\/i>(.*)<\/div>/";
         preg_match($pattern, $pageContent, $matches);
 
